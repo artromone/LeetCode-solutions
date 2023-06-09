0a84449eb3da5172d7a07418dab2de0b0754048c
Artem <artrom170@gmail.com>
2023-06-09 19:47:24 +0300

    Destroyed 🔥

---
+++ 268. Missing Number/good.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+class Solution
+{
+public:
+  int missingNumber(std::vector< int > &nums)
+  {
+    int n = nums.size();
+    int XOR_total = 0;
+    int XOR_nums = 0;
+    for (int i = 0; i <= n; ++i)
+    {
+      XOR_total ^= i;
+    }
+    for (int num: nums)
+    {
+      XOR_nums ^= num;
+    }
+    return XOR_total ^ XOR_nums;
+  }
+};
