84ddcd7b9aab855e115fb76b213ce097d7a22b4a
Artem <artrom170@gmail.com>
2023-06-09 16:41:09 +0300

    Destroyed 🔥

---
+++ 153. Find Minimum in Rotated Sorted Array/good.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+class Solution
+{
+public:
+  int findMin(std::vector< int > &nums)
+  {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right)
+    {
+      int mid = left + (right - left) / 2;
+      // 3 4 5 1 2
+      // 2 3 4 5 1
+      if (nums[mid] > nums[right])
+      {
+        left = mid + 1;
+      }
+      else
+      {
+        right = mid;
+      }
+    }
+    return nums[left];
+  }
+};
